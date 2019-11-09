42ac3065cd5ac8e6173ee02718a2a3ca9b30d91f
lstewart-git <lstewart.it@gmail.com>
2019-11-09 15:09:23 -0500

    frame cap working

--- cam.py
+++ cam.py
@@ -2,17 +2,28 @@
 
 import cv2
 camera = cv2.VideoCapture(0)
+# set resolution
+camera.set(3, 1280)
+camera.set(4, 720)
 
+print('\nThis program will capture a frame from the webcam\n')
+print('press the "s" key to capture and quit\n')
+crpv = input("<ENTER> to start...")
+
+im_ct = 0
 while True:
     return_value,image = camera.read()
 
     gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 
-    cv2.imshow('Press "s" to save',gray)
-
+    cv2.imshow('Press "s" to save',image)
+    filename = 'doggers/' + str(im_ct) + 'cardogger.jpg'
     if cv2.waitKey(1)& 0xFF == ord('s'):
-        cv2.imwrite('test.jpg',image)
-        break
+        cv2.imwrite(filename,image)
+        im_ct += 1
+    if cv2.waitKey(1)& 0xFF == ord('q'):
+       break
+# run this once loop broken    
 camera.release()
 cv2.destroyAllWindows()
 
