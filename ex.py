e7143b470e29cbe91eb043a4cc40e2d846717d2e
lstewart-git <lstewart.it@gmail.com>
2019-11-09 11:22:04 -0500

    added cam support

---
+++ ex.py
@@ -0,0 +1,26 @@
+import numpy as np
+import cv2
+import time
+
+cap = cv2.VideoCapture(0)
+
+ct = 0
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Display the resulting frame
+    #cv2.imshow('frame',gray)
+    title = 'Index: ' + str(ct)
+    cv2.imshow('Index: ',frame)
+    ct += 1
+    time.sleep(3.0)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
