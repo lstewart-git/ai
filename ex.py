7343e22496fa51827b0672834deb01e88a4e7f6c
Lawrence Everett Stewart <stewartl@verachem.com>
2019-11-15 17:40:16 -0500

    friday

--- ex.py
+++ ex.py
@@ -1,26 +1,28 @@
-import numpy as np
-import cv2
+"""this is my docstring"""
+
 import time
 
-cap = cv2.VideoCapture(0)
+import cv2
+
+CAP = cv2.VideoCapture(0)
 
 ct = 0
-while(True):
+while True:
     # Capture frame-by-frame
-    ret, frame = cap.read()
+    ret, frame = CAP.read()
 
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # Display the resulting frame
-    #cv2.imshow('frame',gray)
-    title = 'Index: ' + str(ct)
-    cv2.imshow('Index: ',frame)
+    # cv2.imshow('frame',gray)
+    title = "Index: " + str(ct)
+    cv2.imshow("Index: ", frame)
     ct += 1
     time.sleep(3.0)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
 # When everything done, release the capture
-cap.release()
+CAP.release()
 cv2.destroyAllWindows()
