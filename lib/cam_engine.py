a234e1c933457007456f6453f76ca01f731447f2
Lawrence Stewart <stewartl@verachem.com>
2019-11-29 01:17:33 -0500

    now pull webcam

--- lib/cam_engine.py
+++ lib/cam_engine.py
@@ -10,21 +10,21 @@ class cam_engine(object):
         # set resolution
         ##camera.set(3, 1920)
         #camera.set(4, 1080)
-        camera.set(3, 1280)
-        camera.set(4, 720)
+        self.camera.set(3, 1280)
+        self.camera.set(4, 720)
 
-    def get_image():
+    def get_image(self):
         #get an image for analysis
         filename = 'capture.jpg'
         while True:
-            return_value,image = camera.read()
+            return_value,image = self.camera.read()
             cv2.imshow('Press "s" to capture image',image)
             if cv2.waitKey(1)& 0xFF == ord('s'):
                 cv2.imwrite(filename,image)
                 break
 
         # run this once loop broken    
-        camera.release()
+        self.camera.release()
         cv2.destroyAllWindows()
         return image
 
